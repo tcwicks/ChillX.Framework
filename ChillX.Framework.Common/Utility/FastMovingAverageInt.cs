1bab49e4bd4e63d29246c2fb1cbe207c5ee6bc7c
TCWicks <tcwicks@gmail.com>
2017-01-19 03:47:12 +1300

    Transport connection base class draft and fast moving average algorithm for dynamic latency calculation.

---
+++ ChillX.Framework.Common/Utility/FastMovingAverageInt.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChillX.Framework.Common.Utility
+{
+    /// <summary>
+    /// Fast low CPU usage moving average based on integer math
+    /// Note: This algorithm cannot be implemented for floating point because of cumulative floating point error
+    /// </summary>
+    public class FastMovingAverageInt
+    {
+        /// <summary>
+        /// Initializes moving average for specified window size
+        /// </summary>
+        /// <param name="_WindowSize">Size of moving average windows</param>
+        public FastMovingAverageInt(int _WindowSize)
+        {
+            if (_WindowSize < 2)
+            {
+                _WindowSize = 2;
+            }
+            m_WindowSize = _WindowSize;
+        }
+        private object SyncRoot = new object();
+        private Queue<int> Buffer = new Queue<int>();
+        private int m_WindowSize;
+        private int m_MovingAverage = 0;
+        private int MovingSum = 0;
+        private bool BufferFull;
+
+        /// <summary>
+        /// Calculated moving average
+        /// </summary>
+        public int MovingAverage
+        {
+            get
+            {
+                lock (SyncRoot)
+                {
+                    return m_MovingAverage;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Size of moving average window set by constructor during intialization
+        /// </summary>
+        public int WindowSize
+        {
+            get
+            {
+                return m_WindowSize;
+            }
+        }
+
+        /// <summary>
+        /// Add new value to sequence and recalculate moving average seee <see cref="MovingAverage"/>
+        /// </summary>
+        /// <param name="NewValue">New value to be added</param>
+        public void AddValue(int NewValue)
+        {
+            lock (SyncRoot)
+            {
+                Buffer.Enqueue(NewValue);
+                MovingSum += NewValue;
+                if (!BufferFull)
+                {
+                    int BufferSize = Buffer.Count;
+                    BufferFull = BufferSize == WindowSize;
+                    m_MovingAverage = MovingSum / BufferSize;
+                }
+                else
+                {
+                    MovingSum -= Buffer.Dequeue();
+                    m_MovingAverage = MovingSum / WindowSize;
+                }
+            }
+        }
+    }
+}
--- ChillX.Framework.Transport/TransportConnectionBase.cs
+++ ChillX.Framework.Transport/TransportConnectionBase.cs
@@ -1,4 +1,5 @@
-﻿using ChillX.Framework.Common.WorkItem;
+﻿using ChillX.Framework.Common.Utility;
+using ChillX.Framework.Common.WorkItem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ChillX.Framework.Transport
 {
-    public abstract class TransportConnectionBase
+    public abstract class TransportConnectionBase : IDisposable
     {
         /// <summary>
         /// Constructor iniitilizes this instance with specified WorkItemHandler for recieved work items
@@ -21,6 +22,8 @@ namespace ChillX.Framework.Transport
         private WorkItemHandler RECV;
         private bool m_IsReady = false;
         private bool m_IsTerminated = false;
+        private Byte m_TransportCost = 0;
+        private FastMovingAverageInt LatencyMovingAverage = new FastMovingAverageInt(10);
 
         /// <summary>
         /// Is ready for transmit and recieve functions.
@@ -52,6 +55,31 @@ namespace ChillX.Framework.Transport
         }
 
         /// <summary>
+        /// Transport cost wieghting. Will be used by transport server when multiple transports are available for a given destination.
+        /// Lower weight transports will be prefered over higher weight transports.
+        /// </summary>
+        public Byte TransportCost
+        {
+            get
+            {
+                return m_TransportCost;
+            }
+        }
+
+        /// <summary>
+        /// Xmit latency of this connection. Moving average of last 10 transmissions
+        /// Note: This value is used by the transport server when multiple transports are available for a given destination.
+        /// For the same transport cost see <see cref="TransportCost"/> Lower latency transports will be prefered over higher latency transports.
+        /// </summary>
+        public int LatencyMS
+        {
+            get
+            {
+                return LatencyMovingAverage.MovingAverage;
+            }
+        }
+
+        /// <summary>
         /// Should be called by concrete class to signal connection is ready for processing messages
         /// </summary>
         /// <param name="_IsReady">Is Ready Status</param>
@@ -77,18 +105,36 @@ namespace ChillX.Framework.Transport
         }
 
         /// <summary>
+        /// Should be called by concrete class during initialization to set transport cost value see <see cref="TransportCost"/>
+        /// </summary>
+        /// <param name="Cost"></param>
+        protected void AssignTransportCost(Byte Cost)
+        {
+            m_TransportCost = Cost;
+        }
+
+        /// <summary>
         /// Transmit work item to remote end
         /// </summary>
         /// <param name="WorkItemInstance">Work item to be transmitted</param>
         /// <returns>Success / Failure</returns>
         public bool XMIT(IWorkItem WorkItemInstance)
         {
-            if (IsReady) { return OnXMIT(WorkItemInstance); }
+            if (IsReady)
+            {
+                bool Result;
+                DateTime StartTime = DateTime.Now;
+                Result = OnXMIT(WorkItemInstance);
+                LatencyMovingAverage.AddValue(Convert.ToInt32(DateTime.Now.Subtract(StartTime).TotalMilliseconds));
+            }
             return false;
         }
 
         /// <summary>
         /// Abstract method for concrete class override to implement logic for transmitting work item to remote end
+        /// Note: This method should NOT implement async behavior. For Async capable transports the transport server will initiate multiple parallel conecctions
+        /// Note: This class should wrap a single end to end connection.
+        /// Reason: Atomic message transmission and latency calculation depends on this.
         /// </summary>
         /// <param name="WorkItemInstance">Work item to be transmitted</param>
         /// <returns>Success / Failure</returns>
@@ -104,5 +150,24 @@ namespace ChillX.Framework.Transport
         {
             return RECV(WorkItemInstance);
         }
+
+        private bool m_IsDisposed = false;
+
+        /// <summary>
+        /// Dispose will be called by Transport server once connection is marked terminated by concrete class
+        /// or if the transport server is closing the connection. Will also be called when the transport server is shutting down.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!m_IsDisposed)
+            {
+                DoDispose();
+            }
+        }
+
+        /// <summary>
+        /// Override to be implemented in concrete class for cleanup.
+        /// </summary>
+        protected abstract void DoDispose();
     }
 }
